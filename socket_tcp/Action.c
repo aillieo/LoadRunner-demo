cb4207e7ec7b8d3f9cc38a910265ae3e7cf23d83
= <aillieo@163.com>
2016-11-28 11:09:08 +0800

    update: add a user function

--- socket_tcp/Action.c
+++ socket_tcp/Action.c
@@ -8,7 +8,7 @@ lrs_create_socket("socket0", "TCP", "LocalHost=0", "RemoteHost=SERVER:400", LrsL
 
 lrs_send("socket0", "buf0", LrsLastArg);
 
-lrs_receive("socket0", "buf1", LrsLastArg);
+auto_receive("socket0", "buf1", LrsLastArg);
 
 lrs_close_socket("socket0");
 
--- socket_tcp/utils.h
+++ socket_tcp/utils.h
@@ -17,3 +17,66 @@ int strToHex(char* src, char* des)
 
 
 
+//自定义函数 接收可变长度的消息 具体可以根据协议调整
+//1、接收消息的前四个字节 并将接收的字节转化为十进制数
+//2、根据前四个字节读取到的长度信息 接收消息剩余部分
+int auto_receive(char *sock_desc, char *buf_desc,void *dummy)
+{
+	int rc;
+	
+	int buf_len = 4;
+	
+	char szBytesLength[30], *buf = NULL, *pszError, *pszLastChar;
+
+	rc = lrs_receive_ex(sock_desc, buf_desc, "NumberOfBytesToRecv=4", LrsLastArg);	
+
+	if (rc != 0) 
+	{
+		lr_error_message("Receive 4 bytes failed. The error code = %d", rc);
+
+		return -1;
+	} 
+
+	//判断前4个字节是否接受成功
+	lrs_get_last_received_buffer(sock_desc, &buf, &buf_len);
+
+	if (buf == NULL || buf_len != 4)
+	{
+		lr_error_message("receive of %s failed", buf_desc);
+
+		return -1;
+	}
+
+	sprintf (szBytesLength, "NumberOfBytesToRecv=%d", fiFromHexBinToInt(buf)); 
+
+	lr_debug_message(LR_MSG_CLASS_FULL_TRACE, "!!!! Bytes length = %s", szBytesLength);
+
+	rc = lrs_receive_ex(sock_desc, buf_desc, szBytesLength, LrsLastArg);
+
+	if (rc != 0)
+		return -1;
+
+	return 0;
+}
+
+
+//将接收到的4个字节转化为十进制整数并返回
+int fiFromHexBinToInt(char *szBuffer)
+{
+
+	int i, j, iIntValue = 0, iExp = 1; 
+
+
+	for(i = 0 ; i < 4 ; i ++)
+	{
+		iExp = 1;
+		for(j = i ; j <3 ; j ++ )
+		{
+			iExp*= 256;
+		}
+		iIntValue += (szBuffer[i] & 0x000000ff)  * iExp;
+	}
+
+	return iIntValue;
+
+}
