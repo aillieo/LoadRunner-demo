e58c2d7c54e2b4023f02938b0d0f8bd8b2573ba0
= <aillieo@163.com>
2017-01-08 12:03:18 +0800

    update

--- lr_demo/utils.h
+++ lr_demo/utils.h
@@ -131,7 +131,7 @@ int custom_lrs_receive_all(char *sock_desc, char *buf_desc, void *dummy)
 	//判断首条是否接收成功
 	if(i == -1)
 	{
-		//lr_error_message("接收消息失败");
+		lr_error_message("接收消息失败");
 		return -1;
 	}
 
